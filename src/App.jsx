7a70d06054c4698545bbaee615ebbe27e5dc86e3
sajansinghthakuri <237443145+sajansinghthakuri@users.noreply.github.com>
2026-07-06 17:16:00 +0545

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <main>
+      <div class="flex items-center justify-center h-screen">
+        <h1 className='text-5xl text-violet-500 font-serif text-center'>Welcome to Zentry Game Website.</h1>
+      </div>
+    </main>
+  )
+}
+
+export default App
